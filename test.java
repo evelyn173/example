ad89cd7118ea963034f42a51dd761c62c36327fe
游欣宜 <imocllyee@youxinyis-MacBook-Air.local>
2022-11-30 19:13:55 +0800

    add arithmeti of a&b

--- test.java
+++ test.java
@@ -2,14 +2,12 @@ public class
 {
     public static void main(String args[])
     {
-        System.out.println("Hello World");
-        
-        int sum=0;
-        for(int i=0; i<20; i++)
-        {
-            sum=sum+i;
-        }
-        System.out.println(sum);
+       int a=25
+       int b=5;
+       System.out.println(a+b);
+        System.out.println(a-b);
+         System.out.println(a*b);
+          System.out.println(a/b);
 
     }
 }
