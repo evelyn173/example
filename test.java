c3b3b39ca04dd5eaaa5200d0ab2fbc2a2932b8b8
游欣宜 <imocllyee@youxinyis-MacBook-Air.local>
2022-11-30 19:08:19 +0800

    add the sum of 1+...+20

--- test.java
+++ test.java
@@ -3,5 +3,13 @@ public class
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        
+        int sum=0;
+        for(int i=0; i<20; i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
     }
 }
