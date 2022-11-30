d0ae9692f3d7df59faccc4f171e07f940867eddc
游欣宜 <imocllyee@youxinyis-MacBook-Air.local>
2022-11-30 19:51:17 +0800

    test: print have a nice day

--- test.java
+++ test.java
@@ -2,12 +2,6 @@ public class
 {
     public static void main(String args[])
     {
-       int a=25
-       int b=5;
-       System.out.println(a+b);
-        System.out.println(a-b);
-         System.out.println(a*b);
-          System.out.println(a/b);
-
+       System.out.println("Have a nice day");
     }
 }
